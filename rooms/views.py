a86e0745bc3adb28b88fa7355b2e65a6d0a02439
iheehee <iheeheedev@gmail.com>
2022-06-22 15:28:57 +0900

    3days

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,45 @@
+from ast import Try
 from urllib import response
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import ReadRoomSerializer, WriteRoomSerializer
 
 
-class ListRoomsView(ListAPIView):
+# class ListRoomsView(ListAPIView):
 
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+#    queryset = Room.objects.all()
+#    serializer_class = RoomSerializer
+class RoomsView(APIView):
+    def get(self, request):
+        rooms = Room.objects.all()[:5]
+        serializer = ReadRoomSerializer(rooms, many=True).data
+        return Response(serializer)
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        serializer = WriteRoomSerializer(data=request.data)
+        if serializer.is_valid():
+            room = serializer.save(user=request.user)
+            room_serializer = ReadRoomSerializer(room)
+            return Response(date=room_serializer, status=status.HTTP_200_OK)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RoomView(APIView):
+    def get(self, request, pk):
+        try:
+            room = Room.objects.get(pk=pk)
+            serializer = ReadRoomSerializer(room).data
+        except Room.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
