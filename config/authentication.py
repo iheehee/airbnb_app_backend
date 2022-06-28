ac7f004add6c0e94512b3dfa2b91b29ecf353a4e
iheehee <iheeheedev@gmail.com>
2022-06-28 16:27:11 +0900

    5days

--- config/authentication.py
+++ config/authentication.py
@@ -3,6 +3,6 @@ from rest_framework import authentication
 from rest_framework import exceptions
 
 
-class ExampleAuthentication(authentication.BaseAuthentication):
+class JWTAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         username = request.META.get("HTTP_X_USERNAME")
